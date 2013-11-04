2131e01eacf534b74795e86d93789a03987c2ca5
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-04 12:26:03 +0100

    require login, adding tbs to sessions#new

--- spec/features/users_authentication_spec.rb
+++ spec/features/users_authentication_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 feature "Users authentication" do
   given(:user) {create(:user)}
-  background { login user }
 
-  scenario "user login" do
+  scenario "user must login" do
+    visit root_path
+    page.should have_content "devez vous connecter"
+    login user
     page.should have_content /connecté/i
   end
 
   scenario "users cannot login with incorrect credentials" do
-    visit logout_path
-    page.should have_content /déconnecté/i
     login User.new
     page.should have_content /identifiant ou mot de passe incorrect/i
   end
