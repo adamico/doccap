e6ed603c9d72e68e2b25573cb2a7c8a9b4dc5875
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-04 10:43:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doccap::Application.config.secret_key_base = '8413a78b216480f612b6a11de9513fe0eff2890159d4381ab981ca16ea721ba769d55f1523d5fdefe9e0b8c958fc96621004823e0e1040a7493fc3ef10eb6f87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Doccap::Application.config.session_store :cookie_store, key: '_doccap_session'
