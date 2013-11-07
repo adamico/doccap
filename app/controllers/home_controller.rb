b15579a4d32ab6cdcf414f736dddac40e93ad99b
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-07 15:25:13 +0100

    extracting variables in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,9 @@ class HomeController < ApplicationController
   end
 
   def search
+    vigitox = "http://vigitox.herokuapp.com"
+    vigitox_local = "http://localhost:3001"
+    @vigitox = vigitox
+    @articles = HTTParty.get("#{@vigitox}/api/v1/articles?q=#{params['query']}")
   end
 end
