b327d6bcc05c360647633ce9b6c131142a64aaf6
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-22 15:48:41 +0100

    whitespace, removing local vigitox mentions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   respond_to :js, only: :search
+
   def index
   end
 
   def search
     vigitox = "http://vigitox.herokuapp.com"
-    vigitox_local = "http://localhost:3001"
     @vigitox = vigitox
     @articles = HTTParty.get("#{@vigitox}/api/v1/articles?q=#{params['query']}")
   end
