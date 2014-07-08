3889603cc8f845a6b248f898583b1792139bdac0
Andrea D'Amico <andrea.damico@gmail.com>
2014-07-08 15:52:27 +0200

    fix pages helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,5 @@
 module PagesHelper
   def nested_page_path(page)
-    "/" + ([page] + page.ancestors).reverse.map(&:to_param).join("/")
+    "/" + (page.ancestors + [page]).map(&:to_param).join("/")
   end
 end
